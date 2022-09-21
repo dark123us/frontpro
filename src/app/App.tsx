b397d030e4bd89c7734e3a2e390aef3a8c2b4e7e
Vadzim Pryhodzich <7676562@gmail.com>
2022-09-21 11:49:56 +0300

    11 lesson

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,9 @@
-import {Link, Route, Routes} from "react-router-dom";
-import {Suspense} from 'react';
+import {Link} from "react-router-dom";
 import './styles/index.scss';
 
 import {classNames} from "shared/lib/classNames/classNames";
-import {AboutPage} from "pages/AboutPage";
-import {MainPage} from "pages/MainPage";
 import {useTheme} from "./providers/ThemeProvider";
+import {AppRouter} from "app/providers/router";
 
 
 
@@ -15,18 +13,10 @@ const App = () => {
     return(
         <div className={classNames("app", {}, [theme])}>
             <button onClick={changeTheme}>Change Theme</button>
-
-                    <Link to={'/'}>Home</Link>
-                    <Link to={'/about'}>About</Link>
-                    <Suspense fallback={<div>Loading...</div>} >
-                        <Routes>
-                            <Route path='/about' element={<AboutPage />}/>
-                            <Route path='/' element={<MainPage />}/>
-                        </Routes>
-                    </Suspense>
-
+            <Link to={'/'}>Home</Link>
+            <Link to={'/about'}>About</Link>
+            <AppRouter />
         </div>
-
     )
 }
 
---
+++ src/shared/config/routeConfig/routeConfig.tsx
@@ -0,0 +1,25 @@
+import {RouteProps} from "react-router-dom";
+import {MainPage} from "pages/MainPage";
+import {AboutPage} from "pages/AboutPage";
+
+const enum AppRoutes {
+    MAIN = 'main',
+    ABOUT = 'about'
+}
+
+export const RoutePath: Record<AppRoutes, string> = {
+    [AppRoutes.MAIN]: '/',
+    [AppRoutes.ABOUT]: '/about'
+}
+
+export const RouteConfig: Record<AppRoutes, RouteProps> = {
+    [AppRoutes.MAIN]: {
+        path: RoutePath[AppRoutes.MAIN],
+        element: <MainPage />
+    },
+    [AppRoutes.ABOUT]: {
+        path: RoutePath[AppRoutes.ABOUT],
+        element: <AboutPage />
+
+    }
+}
