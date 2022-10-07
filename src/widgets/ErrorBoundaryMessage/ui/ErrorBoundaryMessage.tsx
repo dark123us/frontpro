fea2661ae46ed6813d62bc04f369204783de40a4
Vadzim Pryhodzich <7676562@gmail.com>
2022-10-07 18:43:30 +0300

    27 first ci, fix err, add tests

--- src/widgets/ErrorBoundaryMessage/ui/ErrorBoundaryMessage.tsx
+++ src/widgets/ErrorBoundaryMessage/ui/ErrorBoundaryMessage.tsx
@@ -2,7 +2,6 @@ import { FC } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
 import { Button } from 'widgets/Button';
-import cls from './ErrorBoundaryMessage.module.scss';
 
 interface ErrorBoundaryMessageProps {
     className?: string
@@ -16,7 +15,7 @@ export const ErrorBoundaryMessage: FC<ErrorBoundaryMessageProps> = (props) => {
         location.reload();
     };
     return (
-        <div className={classNames(cls.errorBoundaryMessage, {}, [className])}>
+        <div className={classNames('', {}, [className])}>
             <h1>{t('Что-то пошло не так')}</h1>
             <Button onClick={onReload}>{t('reload page')}</Button>
         </div>
