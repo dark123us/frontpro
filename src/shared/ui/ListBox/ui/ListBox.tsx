b4501d7cf51e87d056551b5e196e7b1d80737a3e
Vadzim Pryhodzich <7676562@gmail.com>
2022-12-22 18:42:34 +0300

    67 завершил оформление Listbox - внедряю в Card

--- src/shared/ui/ListBox/ui/ListBox.tsx
+++ src/shared/ui/ListBox/ui/ListBox.tsx
@@ -1,39 +1,73 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
-import { Fragment, memo, ReactNode } from 'react';
-import { ListBox as HListBox } from '@headlessui/react';
-import { Button } from 'shared/ui/Button';
+import {
+    Fragment, memo, ReactNode, useState,
+} from 'react';
+import { Listbox as HListBox } from '@headlessui/react';
+import { Button } from '../../Button';
 import cls from './ListBox.module.scss';
 
+export interface ListBoxItem {
+    value: string
+    content: ReactNode
+    disabled?: boolean
+}
+
 interface ListBoxProps {
+    items?: ListBoxItem[]
     className?: string;
-    children?: ReactNode;
+    value?: string
+    defaultValue?: string
+    onChange: <T extends string>(value: T) => void
+
 }
 
 export const ListBox = memo((props: ListBoxProps) => {
     const {
         className,
-        children,
+        items,
+        defaultValue,
+        value,
+        onChange,
     } = props;
+
+    const [selected, setSelected] = useState<ListBoxItem>();
+
     const { t } = useTranslation();
     return (
         <HListBox
             as="div"
             className={classNames(cls.ListBox, {}, [className])}
-            value={selectedPerson}
-            onChange={setSelectedPerson}
+            value={value}
+            onChange={onChange}
         >
             <HListBox.Button className={cls.trigger}>
                 <Button>
-                    {t('click')}
+                    {value ?? defaultValue}
                 </Button>
             </HListBox.Button>
-            <HListBox.Options classname={cls.options}>
-                <HListBox.Option
-                    key={person.id}
-                    value={person}
-                    className={cls.item}
-                />
+            <HListBox.Options className={cls.options}>
+                {items?.map((item) => (
+                    <HListBox.Option
+                        as={Fragment}
+                        key={item.value}
+                        value={item.value}
+                        disabled={item.disabled}
+                    >
+                        {({ active, selected }) => (
+                            <li
+                                className={classNames(cls.item, {
+                                    [cls.active]: active,
+                                    [cls.disabled]: item.disabled,
+
+                                })}
+                            >
+                                {selected && '!!!'}
+                            </li>
+                        )}
+                    </HListBox.Option>
+                ))}
+
             </HListBox.Options>
         </HListBox>
     );
