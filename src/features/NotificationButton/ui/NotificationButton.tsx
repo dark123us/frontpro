600c71a26cc2057afcf8bba4fcb7abdac91f0354
Vadzim Pryhodzich <7676562@gmail.com>
2023-01-05 19:49:20 +0300

    80 допилил вывод под мобилку, поправил ошибки

--- src/features/NotificationButton/ui/NotificationButton.tsx
+++ src/features/NotificationButton/ui/NotificationButton.tsx
@@ -1,11 +1,14 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
-import { memo, ReactNode } from 'react';
+import {
+    memo, ReactNode, useCallback, useState,
+} from 'react';
 import { Button, ButtonTheme } from 'shared/ui/Button';
 import { Icon } from 'shared/ui/Icon';
 import NotificationIcon from 'shared/assets/icons/notification-20-20.svg';
 import { NotificationList } from 'entities/Notification';
 import { Popover } from 'shared/ui/Popups';
+import { Drawer } from 'shared/ui/Drawer';
 import cls from './NotificationButton.module.scss';
 
 interface NotificationButtonProps {
@@ -19,16 +22,47 @@ export const NotificationButton = memo((props: NotificationButtonProps) => {
         children,
     } = props;
     const { t } = useTranslation();
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    const onOpenDrawer = useCallback(() => {
+        setIsOpen(true);
+    }, [setIsOpen]);
+
+    const onCloseDrawer = useCallback(() => {
+        setIsOpen(false);
+    }, [setIsOpen]);
+
+    const detectDevice = () => {
+        const isMobile = window.matchMedia;
+        if (!isMobile) return false;
+
+        const device = isMobile('(pointer:coarse)');
+        return device.matches;
+    };
+
+    const trigger = (
+        <Button theme={ButtonTheme.CLEAR} onClick={onOpenDrawer}>
+            <Icon Svg={NotificationIcon} inverted />
+        </Button>
+    );
+
+    if (detectDevice()) {
+        return (
+            <>
+                {trigger}
+                <Drawer isOpen={isOpen} onClose={onCloseDrawer}>
+                    <NotificationList />
+                </Drawer>
+            </>
+        );
+    }
+
     return (
         <Popover
             className={classNames(cls.NotificationButton, {}, [className])}
             direction="bottom left"
-            trigger={(
-                <Button theme={ButtonTheme.CLEAR}>
-                    <Icon Svg={NotificationIcon} inverted />
-                </Button>
-            )}
-
+            trigger={trigger}
         >
             <NotificationList className={cls.notifications} />
         </Popover>
