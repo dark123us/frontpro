ca302269029e03586a2a2dca00401a4e125a2160
Vadzim Pryhodzich <7676562@gmail.com>
2023-01-11 20:09:03 +0300

    112 возможная проблема со сторибуком, на всяк случай комент оставил 113, начало по cypress

---
+++ cypress/support/commands.ts
@@ -0,0 +1,17 @@
+Cypress.Commands.add('login', (email, password) => {
+    //
+});
+declare global {
+  namespace Cypress {
+    interface Chainable {
+      login(email: string, password: string): Chainable<void>
+      drag(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
+      dismiss(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
+
+      // @ts-ignore
+      visit(originalFn: CommandOriginalFn, url: string, options: Partial<VisitOptions>): Chainable<Element>
+    }
+  }
+}
+
+export {};
